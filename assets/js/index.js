7a0e6d59e67b74e2e67092fb027513cf88e3d6af
nikhil.mate <nikhil.mate>
2019-06-19 17:42:22 +0530

    viewport

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,9 @@ var trigger = document.querySelectorAll("a.cta-trigger");
 for (let i = 0; i < trigger.length; i++) {
   var element = trigger[i];
   element.addEventListener("click", function(e){
+    e.preventDefault();
     console.log(e.target.getAttribute("data-value"));
   });
 }
 
-// trigger.forEach(btn => {
-//   btn.addEventListener("click", function(e){
-//     console.log(e.target.getAttribute("data-value"));
-//   })
-// });
 

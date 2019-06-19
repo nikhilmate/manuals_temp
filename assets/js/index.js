00b00dd1f69091bd1240051fc05a59a64b921f21
nikhil.mate <nikhil.mate>
2019-06-19 15:34:59 +0530

    day 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 var trigger = document.querySelectorAll("a.cta-trigger");
-console.log(trigger);
-// trigger.forEach(btn => btn.addEventListener("click", function(e) {
-//   e.preventDefault();
-//   console.log(btn.getAttribute("data-value"));
-  
-// }));
+//console.log(typeof trigger);
+trigger.forEach(btn => {
+  btn.addEventListener("click", function(e){
+    console.log(e.target.getAttribute("data-value"));
+  })
+});
 

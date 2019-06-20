f5e717818a2db6ac4ae2d6e3fd1ab2fc486c467e
nikhil.mate <nikhil.mate>
2019-06-20 15:05:39 +0530

    all bugs removed v2

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,25 +9,25 @@ for (var i = 0; i < trigger.length; i++) {
 }
 
 
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
 // Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// // Safari 3.0+ "[object HTMLElementConstructor]" 
+// var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// // Internet Explorer 6-11
+// var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
+// // Edge 20+
+// var isEdge = !isIE && !!window.StyleMedia;
 
-// Chrome 1 - 71
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+// // Chrome 1 - 71
+// var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
+// // Blink engine detection
+// var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 if (isFirefox) {
   var removed = document.querySelector(".chrome-desclaimer");

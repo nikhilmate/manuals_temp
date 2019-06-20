376c2b95c0f36e52d3bb46e2df7185fa8d6c45f5
nikhil.mate <nikhil.mate>
2019-06-20 13:49:21 +0530

    6 bug solved

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 var trigger = document.querySelectorAll("a.cta-trigger");
 
-for (let i = 0; i < trigger.length; i++) {
+for (var i = 0; i < trigger.length; i++) {
   var element = trigger[i];
   element.addEventListener("click", function(e){
     e.preventDefault();
